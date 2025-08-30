e9fa986a73452cc92763b1065781dd8d27023ec8
StoneAgeDev <ByteSpecter64@proton.me>
2025-08-31 00:18:19 +0200

    feature update: sidebar and resizeable regions not working due to me hating CSS layout. fix tomorrow

---
+++ template/src/lib/components/app/ResizeShell.svelte
@@ -0,0 +1,14 @@
+<!-- template\src\lib\components\app\ResizeShell.svelte -->
+<script lang="ts">
+	import * as Resizable from '$lib/components/ui/resizable/index.js';
+	import MainFrameShell from './MainFrameShell.svelte';
+	import SidebarShell from './SidebarShell.svelte';
+</script>
+
+<Resizable.PaneGroup direction="horizontal" class="h-full rounded-lg border">
+	<Resizable.Pane defaultSize={25}>
+		<div class="m-28 h-full w-full border-2"></div>
+	</Resizable.Pane>
+	<Resizable.Handle withHandle />
+	<Resizable.Pane defaultSize={75}></Resizable.Pane>
+</Resizable.PaneGroup>
--- template/src/routes/+layout.svelte
+++ template/src/routes/+layout.svelte
@@ -1,10 +1,17 @@
+<!-- template/src/routes/+layout.svelte -->
 <script lang="ts">
-	import ColorToggle from './../lib/components/app/ColorToggle.svelte';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import ResizeShell from '$lib/components/app/ResizeShell.svelte';
 	let { children } = $props();
 </script>
 
-<ColorToggle />
-<ModeWatcher />
-{@render children?.()}
+<!-- 👇 anchor to viewport height -->
+<div class="flex h-dvh w-full flex-col">
+	<ModeWatcher />
+	<!-- 👇 let shell consume remaining space -->
+	<div class="min-h-0 flex-1">
+		<ResizeShell />
+	</div>
+	{@render children?.()}
+</div>
