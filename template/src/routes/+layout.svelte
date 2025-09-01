8e31c3c95d4cb18d48638c7e8760b9195bbb2a7a
StoneAgeDev <ByteSpecter64@proton.me>
2025-09-01 18:12:44 +0200

    Scratched Resizable Sidebar - provided new dashboard template demoes instead.

--- template/src/routes/+layout.svelte
+++ template/src/routes/+layout.svelte
@@ -2,11 +2,10 @@
 <script lang="ts">
 	import '../app.css'
 	import { ModeWatcher } from 'mode-watcher'
-	import ResizeShell from '$lib/components/app/ResizeShell.svelte'
+
 	let { children } = $props()
 </script>
 
 <ModeWatcher />
-<ResizeShell />
 
 {@render children?.()}
---
+++ template/src/routes/component-testing/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import RadialDemo from '$lib/components/chart/demo/RadialDemo.svelte'
+</script>
+
+<div class="p-30 flex h-full flex-col">
+	<h1
+		class="text-foreground mb-5 text-xl font-bold">
+		Showcasing component
+	</h1>
+
+	<div
+		class="bg-muted-foreground/10
+           flex flex-1 items-center justify-center
+           rounded-lg p-10
+           shadow-[0_0_10px_rgba(0,0,0,0.5),0_0_20px_rgba(255,255,255,0.05)]
+           ring-1 ring-black/50">
+		<RadialDemo />
+	</div>
+</div>
